a693188f29646c08c78dc0cb8b9d686085152b9b
elfredburger <elfredfredburger@gmail.com>
2024-05-11 15:07:01 +0200

    buyerget

--- src/buyer/buyers.controller.ts
+++ src/buyer/buyers.controller.ts
@@ -1,24 +1,36 @@
-import { Controller, Get, Post, Put, Delete, Param, Patch, Req, Res, Body, UsePipes, ValidationPipe, ParseIntPipe, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Patch,
+  Req,
+  Res,
+  Body,
+  UsePipes,
+  ValidationPipe,
+  ParseIntPipe,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { buyerDto } from 'src/buyer/dto/buyer.dto';
 import { BuyersService } from 'src/buyer/buyers.service';
 import { SubscriptionDto } from '../subscription/dto/subscriprion.dto';
 
-
-
 @Controller('buyers')
-export class BuyerController{
-    constructor (private buyersService: BuyersService){}
-   
-   
-    @Post('create')
-    @UsePipes(new ValidationPipe({whitelist:true}))
-    create(@Body() buyerPayload: buyerDto){
-      
-        return this.buyersService.createBuyer(buyerPayload)
-    }
+export class BuyerController {
+  constructor(private buyersService: BuyersService) {}
 
-    
+  @Post('create')
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  create(@Body() buyerPayload: buyerDto) {
+    return this.buyersService.createBuyer(buyerPayload);
+  }
 
+  @Get('get')
+  getone(@Body() buyerPayload: buyerDto) {
+    return this.buyersService.findOne(buyerPayload.email);
+  }
 }
-
-
