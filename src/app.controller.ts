9d74cc8e7af85c29321ce81e5aa55f869a0dce37
elfredburger <elfredfredburger@gmail.com>
2024-05-11 14:18:48 +0200

    cors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, UseGuards,Post,Request,Response,Get } from '@nestjs/common';
-import {AppService} from './app.service'
+/* eslint-disable prettier/prettier */
+import {
+  Controller,
+  UseGuards,
+  Post,
+  Request,
+  Response,
+  Get,
+} from '@nestjs/common';
+import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local.auth.guard';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthenticatedGuard } from './auth/authenticated.guard';
@@ -7,25 +15,28 @@ import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService:AppService){}
-    
-    @UseGuards(LocalAuthGuard)
-    @Post('login')
-    login(@Request() req):any{
-        return{"logged in":true}
-    }
-    
-    @UseGuards(AuthenticatedGuard)
-    @Get('protected')
-    authTest(@Request() req):any{
-        return {"access":true}
-    }
-    @Get('logout')
-    async logout(@Request() request): Promise<any> {
-        request.session.destroy(() => {
-        return {
-        message: "Logout successful"};}
-        );
-        return {
-            message: "Logout successful"}
-}}
+  constructor(private readonly appService: AppService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): any {
+    return { 'logged in': true };
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('protected')
+  authTest(@Request() req): any {
+    return { access: true };
+  }
+  @Get('logout')
+  async logout(@Request() request): Promise<any> {
+    request.session.destroy(() => {
+      return {
+        message: 'Logout successful',
+      };
+    });
+    return {
+      message: 'Logout successful',
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as session  from 'express-session';
-import *as passport from 'passport';
+import * as session from 'express-session';
+import * as passport from 'passport';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     session({
-      
-      secret:process.env.SECRET_KEY,
-      resave:false,
-      saveUninitializer:false,
-      
-  })
-)
-  app.use(passport.initialize())
-  app.use(passport.session())
+      secret: process.env.SECRET_KEY,
+      resave: false,
+      saveUninitializer: false,
+    }),
+  );
+  app.enableCors();
+  app.use(passport.initialize());
+  app.use(passport.session());
   await app.listen(3000);
-  
-  
 }
 bootstrap();
